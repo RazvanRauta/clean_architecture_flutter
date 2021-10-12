c37b433a708845b6b1606a81ec3ac2b19f9d6388
Razvan Rauta <rrauta@klika-tech.com>
2021-10-12 19:01:34 +0300

    Added random number use case and tests

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,6 +1,10 @@
-import 'package:clean_architecture_flutter/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../error/failures.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
 }
+
+class NoParams extends Equatable {}
